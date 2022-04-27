bd8364619a32f5d9c175a3ac2eb79945ec96dcf8
Vivek Sasidharan <viveks431@gmail.com>
2022-04-27 22:40:33 +0200

    1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,24 @@
 let pokemonList = [];
 
 pokemonList = [
-    { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
-    { name: "Pikachu", height: 2, types: ['electric', 'speed'] },
-    { name: "Charizard", height: 15, types: ['fire', 'flying'] }
+    { name: "Bulbasaur", 
+    height: 0.7, 
+    types: ['grass', 'poison'] 
+},
+{   name: "Squirtle", 
+    height: 1,  
+    types: ['water']    
+},
+{   name: "Charizard",    
+    height: 15, 
+    types: ['fire', 'flying'] 
+}
 ];
-
 document.write('<ul>')
-
 for (let i = 0; i < pokemonList.length; i++) {
     document.write('<li>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')')
     if (pokemonList[i].height > 5) {
         document.write(' - wow this pokemon is big!' + '</li>' + '<br>')
     } else { document.write('</li>' + '<br>') }
 }
-
 document.write('</ul>')
